1c28f9cc409252c8aecd36eb41c7c950d4f640b7
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-04-07 14:35:03 +0300

    serices to try catch

--- src/js/services/serviceDatabase.js
+++ src/js/services/serviceDatabase.js
@@ -7,47 +7,52 @@ const db = firebase.firestore();
 export const fetchWatchedList = async (uid) => {
   const refData = db.collection("watched").doc(uid);
 
-  return await refData
-    .get()
-    .then((doc) => {
+  try {
+    return await refData.get().then((doc) => {
       if (doc.exists) return doc.data().list;
       else return [];
-    })
-    .catch((err) => console.error(err));
+    });
+  } catch (err) {
+    console.error(err);
+    return { status: 500 };
+  }
 };
 
 export const updateWatchedList = async (uid, data) => {
   const refData = db.collection("watched").doc(uid);
 
-  return await refData
-    .get()
-    .then(async (doc) => {
+  try {
+    await refData.get().then(async (doc) => {
       if (doc.exists)
-        await refData
-          .update({
-            list: firebase.firestore.FieldValue.arrayUnion(data),
-          })
-          .then(() => ({ result: "ok" }))
-          .catch((err) => console.error(err));
+        await refData.update({
+          list: firebase.firestore.FieldValue.arrayUnion(data),
+        });
       else
         await db
           .collection("watched")
           .doc(uid)
           .set({ list: [data] })
-          .then(() => ({ result: "ok" }))
-          .catch((err) => console.error(err));
-    })
-    .then(() => ({ status: 200 }))
-    .catch((err) => console.error(err));
+          .then(() => ({ result: "ok" }));
+    });
+
+    return { status: 200 };
+  } catch (err) {
+    console.error(err);
+    return { status: 500 };
+  }
 };
 
 export const removeWatchedList = async (uid, data) => {
   const refData = db.collection("watched").doc(uid);
 
-  await refData
-    .update({
+  try {
+    await refData.update({
       list: firebase.firestore.FieldValue.arrayRemove(data),
-    })
-    .then(() => ({ status: 200 }))
-    .catch((err) => console.error(err));
+    });
+
+    return { status: 200 };
+  } catch (err) {
+    console.error(err);
+    return { status: 500 };
+  }
 };
