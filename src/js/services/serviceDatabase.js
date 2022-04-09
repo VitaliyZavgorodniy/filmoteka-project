d2f43a15d3d1f22726f4bfc4226b12eaa7796399
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-04-09 15:16:27 +0300

    added serviceDB queued

--- src/js/services/serviceDatabase.js
+++ src/js/services/serviceDatabase.js
@@ -56,3 +56,56 @@ export const removeWatchedList = async (uid, data) => {
     return { status: 500 };
   }
 };
+
+export const fetchQueuedList = async (uid) => {
+  const refData = db.collection("queued").doc(uid);
+
+  try {
+    return await refData.get().then((doc) => {
+      if (doc.exists) return doc.data().list;
+      else return [];
+    });
+  } catch (err) {
+    console.error(err);
+    return { status: 500 };
+  }
+};
+
+export const updateQueuedList = async (uid, data) => {
+  const refData = db.collection("queued").doc(uid);
+
+  try {
+    await refData.get().then(async (doc) => {
+      if (doc.exists)
+        await refData.update({
+          list: firebase.firestore.FieldValue.arrayUnion(data),
+        });
+      else
+        await db
+          .collection("queued")
+          .doc(uid)
+          .set({ list: [data] })
+          .then(() => ({ result: "ok" }));
+    });
+
+    return { status: 200 };
+  } catch (err) {
+    console.error(err);
+    return { status: 500 };
+  }
+};
+
+export const removeQueuedList = async (uid, data) => {
+  const refData = db.collection("queued").doc(uid);
+
+  try {
+    await refData.update({
+      list: firebase.firestore.FieldValue.arrayRemove(data),
+    });
+
+    return { status: 200 };
+  } catch (err) {
+    console.error(err);
+    return { status: 500 };
+  }
+};
