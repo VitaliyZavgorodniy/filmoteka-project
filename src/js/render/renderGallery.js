13467f0155afb13c78902f28da620d75a19b0227
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-04-14 11:14:26 +0300

    fix modal markup

--- src/js/render/renderGallery.js
+++ src/js/render/renderGallery.js
@@ -25,8 +25,6 @@ export const renderGallery = (list) => {
 
         const releaseYear = parseYear(release_date);
 
-        console.log(list);
-
         return templateCard(
           id,
           poster_path,
